2b7b61c1a28c157948fd6b13e92bdd2dc049347e
kcwaggs <kcwaggs98@gmail.com>
2024-02-27 19:59:38 -0600

    final push

--- EmployeeSQL/code_and_query/table_creation.sql
+++ EmployeeSQL/code_and_query/table_creation.sql
@@ -3,46 +3,53 @@
 -- departments table: drop table if exists, then creats the table. 
 drop table if exists departments;
 create table departments (
-	dept_no varchar primary key,
-	dept_name varchar
+	dept_no varchar(4) primary key,
+	dept_name varchar(30) not null
 );
 
--- dept_emp table: drop table if exists, then creats the table. 
-drop table if exists dept_emp;
-create table dept_emp (
-	emp_no int primary key,
-	dept_no varchar 
-);
-
--- dept_manager table: drop table if exists, then creats the table. 
-drop table if exists dept_manager;
-create table dept_manager (
-	dept_no varchar primary key,
-	emp_no int 
+-- titles table: drop table if exists, then creats the table. 
+drop table if exists titles;
+create table titles (
+	title_id varchar(5) primary key,
+	title varchar(30) not null
 );
 
 -- employees table: drop table if exists, then creats the table. 
 drop table if exists employees;
 create table employees (
 	emp_no int primary key,
-	emp_title_id varchar,
-	birth_date date,
-	first_name varchar,
-	last_name varchar,
-	sex varchar,
-	hire_date date
+	emp_title_id varchar(5) not null,
+	birth_date date not null,
+	first_name varchar(30) not null,
+	last_name varchar(30) not null,
+	sex varchar(1) not null,
+	hire_date date not null,
+	foreign key (emp_title_id) references titles(title_id)
 );
 
 -- salaries table: drop table if exists, then creats the table. 
 drop table if exists salaries;
 create table salaries (
 	emp_no int primary key,
-	salary int
+	salary int not null,
+	foreign key (emp_no) references employees(emp_no)
 );
 
--- titles table: drop table if exists, then creats the table. 
-drop table if exists titles;
-create table titles (
-	title_id varchar primary key,
-	title varchar
+-- dept_emp table: drop table if exists, then creats the table. 
+drop table if exists dept_emp;
+create table dept_emp (
+	emp_no int,
+	dept_no varchar(4),
+	primary key (emp_no, dept_no),
+	foreign key (emp_no) references employees(emp_no),
+	foreign key (dept_no) references departments(dept_no)
+);
+
+-- dept_manager table: drop table if exists, then creats the table. 
+drop table if exists dept_manager;
+create table dept_manager (
+	dept_no varchar(4) not null,
+	emp_no int primary key,
+	foreign key (dept_no) references departments(dept_no),
+	foreign key (emp_no) references employees(emp_no)
 );
